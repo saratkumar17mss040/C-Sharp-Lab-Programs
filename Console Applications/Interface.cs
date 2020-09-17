8e81f7afd854d0641b7ec464a0baef572be285f0
sarath <sarathdon9245@gmail.com>
2020-09-17 10:24:28 +0530

    🚀 better interface example

--- Console Applications/Interface.cs
+++ Console Applications/Interface.cs
@@ -1,46 +1,82 @@
 using System;
 
-interface myOsDetails {
-    void getMyOsDetails();
-}
-
-interface myFriendOsDetails {
-     void getMyFriendOsDetails();
-}
-
-class myOs : myOsDetails {
-    public void getMyOsDetails() {
-        Console.WriteLine("Edition: Windows 10 Home Single Language");
-        Console.WriteLine("Version: 1909");
-    }
-}
-
-class myFriendOs : myFriendOsDetails {
-     public void getMyFriendOsDetails() {
-        Console.WriteLine("Edition: Windows 10 Home Multi Language");
-        Console.WriteLine("Version: 1809");
-    }
-}
-
-class allOs : myOsDetails, myFriendOsDetails
+interface studentDetails
 {
-    myOs os1 = new myOs();
-    myFriendOs os2 = new myFriendOs();
-    
-    public void getMyOsDetails(){
-        os1.getMyOsDetails();
+    void getStudentDetailsAndDisplay();
+}
+
+interface MarkDetails
+{
+    void getMarkDetailsAndDisplay();
+}
+
+class Student : studentDetails
+{
+    string studentName = "";
+    int age = 0;
+    string rollno = "";
+    public void getStudentDetailsAndDisplay()
+    {
+        Console.WriteLine("Enter Student name:");
+        studentName = Console.ReadLine();
+        Console.WriteLine("Enter Student age:");
+        age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Student rollno:");
+        rollno = Console.ReadLine();
+        Console.WriteLine("Student name: {0}", studentName);
+        Console.WriteLine("Student age: " + age.ToString());
+        Console.WriteLine("Student rollno: " + rollno.ToString());
     }
-    
-    public void getMyFriendOsDetails() {
-        os2.getMyFriendOsDetails();
+
+
+}
+
+class Marks : MarkDetails
+{
+    int c, oracledb, java, python = 0;
+    public void getMarkDetailsAndDisplay()
+    {
+        Console.WriteLine("Enter mark for c:");
+        c = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter mark for oracledb:");
+        oracledb = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter mark for java:");
+        java = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter mark for python:");
+        python = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("C mark: {0}", c);
+        Console.WriteLine("Oracle db mark: " + oracledb.ToString());
+        Console.WriteLine("Java mark: " + java.ToString());
+        Console.WriteLine("Python mark: " + python.ToString());
     }
 }
 
-public class InterfaceDemo {
+class Details : studentDetails, MarkDetails
+{
+    Student stu = new Student();
+    Marks mark = new Marks();
+
+    public void getStudentDetailsAndDisplay()
+    {
+        stu.getStudentDetailsAndDisplay();
+    }
+
+    public void getMarkDetailsAndDisplay()
+    {
+        mark.getMarkDetailsAndDisplay();
+    }
+
+}
+
+public class InterfaceDemo
+{
     public static void Main()
     {
-        allOs os = new allOs();
-        os.getMyOsDetails();
-        os.getMyFriendOsDetails();
+        Student stu = new Student();
+        Marks mark = new Marks();
+        Details obj = new Details();
+        obj.getStudentDetailsAndDisplay();
+        obj.getMarkDetailsAndDisplay();
+        Console.Read();
     }
 }
